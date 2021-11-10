75f0273f6de62fb0e7b02a1273774016b5564ff6
reissue <68487086+MinSoo97@users.noreply.github.com>
2021-11-10 13:14:07 +0900

    Update dbconfig.js

--- dbconfig/dbconfig.js
+++ dbconfig/dbconfig.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 var pool = mysql.createPool({  // createPool 속도가 빠름 
     host:"localhost",
     user:"root",
-    password:"alstn123",
+    password:"",
     port:3306,
     database:"bookstroe"
 })
